fb7d36a5c81b132a234e754d62e4c00bb8d488a0
sarthak <sarthak.robo@gmail.com>
2026-06-10 13:43:42 -0700

    vite fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '',
+  base: '/',
   assetsInclude: ['**/*.onnx'],
   optimizeDeps: {
     exclude: ['onnxruntime-web'],
